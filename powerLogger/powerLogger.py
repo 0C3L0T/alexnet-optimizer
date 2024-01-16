93a5bd71393eb05b7e608946079b997e0c341e8a
Merijn <merijn.laks@student.uva.nl>
2024-01-16 16:16:38 +0100

    add parser for adb shell (not tested)

--- powerLogger/powerLogger.py
+++ powerLogger/powerLogger.py
@@ -1,21 +1,38 @@
-import serial 
+import serial
+import time
 
-try: 
-    arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
-except:
-    arduino = serial.Serial(port='/dev/ttyACM1', baudrate=9600, timeout=.1)
+def arduino_listener():
+    # get arduino serial port
+    try: 
+        arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
+    except:
+        arduino = serial.Serial(port='/dev/ttyACM1', baudrate=9600, timeout=.1)
 
+    try:
+        while True: 
+            data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
+            data = data.decode('utf-8')
+            data = data.split(',')
 
-while True: 
-    data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
-    data = data.decode('utf-8')
-    data = data.split(',')
-
-    if len(data) != 3:
-        continue
+            # first output only has two values
+            if len(data) != 3:
+                continue
     
-    [curr_V, curr_mA, power_mW] = data
+            [curr_V, curr_mA, power_mW] = data
 
-    print(f"Voltage: {curr_V}V, Current: {curr_mA}mA, Power: {power_mW}mW (from Python)")
+            timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
+            result = f"[{timestamp}] Voltage: {curr_V}V, Current: {curr_mA}mA, Power: {power_mW}mW\n"
+            print(result)
 
+            with open("power_output.txt", "a") as f:
+                f.write(result)
+        
+    except KeyboardInterrupt:
+        print("Script terminated by user.")
+
+    finally:
+        arduino.close()
+
+if __name__ == "__main__":
+    arduino_listener()
