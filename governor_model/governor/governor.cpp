b3caa7feb94e35caf5108610ef760316f3332565
Merijn <merijn.laks@student.uva.nl>
2024-01-29 17:02:32 +0100

    able to chance frequencies

--- governor_model/governor/governor.cpp
+++ governor_model/governor/governor.cpp
@@ -4,8 +4,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <stdio.h>
+#include <stdlib.h>
 #include "GA.h"
 #include "fitness.h"
+
+using namespace std;
 
 int LittleFrequencyTable[] = {500000, 667000, 1000000, 1200000, 1398000, 1512000, 1608000, 1704000, 1800000};
 int BigFrequencyTable[] = {500000, 667000, 1000000, 1200000, 1398000, 1512000, 1608000, 1704000, 1800000, 1908000, 2016000, 2100000, 2208000};
@@ -48,10 +51,33 @@ int main(int argc, char *argv[]) {
     int pp1 = solution.genes[0]->layers;
     int pp2 = pp1 + solution.genes[1]->layers;
 
+    /* Export OpenCL library path */
+    system("export LD_LIBRARY_PATH=/data/local/Working_dir");
+    setenv("LD_LIBRARY_PATH", "/data/local/Working_dir", 1);
 
-    // optionally: parse feedback (look at exampleGovernor)
+    /* Setup Performance Governor (CPU) */
+    system("echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor");
+    system("echo performance > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor");
 
+    // set little and big frequencies
+    string Command = "";
+    Command = "echo " + to_string(little_freq) + " > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq";
+    system(Command.c_str());
+    Command = "echo " + to_string(big_freq) + " > /sys/devices/system/cpu/cpufreq/policy2/scaling_max_freq";
+    system(Command.c_str());
+
+    // print configuration
     char buffer[256];
     chromosomeToString(&solution, buffer, 256);
     printf("%s", buffer);
+
+    int N_Frames = 20;
+    // run graph
+    char Run_Command[150];
+    sprintf(Run_Command, "./graph_alexnet_pipe_all --threads=4 --threads2=2 --target=NEON --n=%d --partition_point=%d --partition_point2=%d --order=B-G-L > output.txt",
+            N_Frames, pp1, pp2);
+    system(Run_Command);
+
+    // optionally: parse feedback (look at exampleGovernor)
+
 }
